1b3aeee388a6884e7d76e2badfe0cd024a809152
Devin Eldreth <deldreth@emoneyadvisor.com>
2022-09-16 14:53:07 -0400

    chore: update amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "editorvueb03f6108": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "api": {
+        "EditorVue": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "EditorVueStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
